b1c2bf4c50892daa4fbe239e19d7215fdee19ef1
Anup-12345 <anup4867@gmail.com>
2026-08-02 23:06:09 +0530

    add env file

--- myApp.js
+++ myApp.js
@@ -1,13 +1,27 @@
-let express = require("express");
+let express = require('express');
 let app = express();
+require("dotenv").config();
 console.log("Hello World");
+
+// Serve static files
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", function (req, res) {
+
+// Home route
+app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
+
+// JSON route
 app.get("/json", function(req, res) {
+  let message = "Hello json";
+
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = message.toUpperCase();
+  }
+
   res.json({
-    message: "Hello json"
+    message: message
   });
 });
+
 module.exports = app;
